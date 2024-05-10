a643118415fb877824a0ab623b9cbd3afa2f5b7b
Аскат Наринбетов <231308@astanait.edu.kz>
2024-05-10 22:16:37 +0500

    ThirdAssignment

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,5 +1,5 @@
 public class HeapSort {
-    public void sort(int[] arr) {
+    public static void sort(int[] arr) {
         int n = arr.length;
         for (int i = n / 2 - 1; i >= 0; i--)
             Heapify(arr, n, i);
@@ -10,16 +10,14 @@ public class HeapSort {
             Heapify(arr, i, 0);
         }
     }
-    void Heapify(int[] arr, int n, int i) {
+    public static void Heapify(int[] arr, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
         if (left < n && arr[left] > arr[largest])
             largest = left;
-
         if (right < n && arr[right] > arr[largest])
             largest = right;
-
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
@@ -27,9 +25,8 @@ public class HeapSort {
             Heapify(arr, n, largest);
         }
     }
-    static void printArray(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++)
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,16 +1,13 @@
 class MergeSort {
-    void merge(int[] arr, int left, int mid, int right) {
+    public static void Merge(int[] arr, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
-
         int[] L = new int[n1];
         int[] R = new int[n2];
-
         for (int i = 0; i < n1; i++)
             L[i] = arr[left + i];
         for (int j = 0; j < n2; j++)
             R[j] = arr[mid + 1 + j];
-
         int i = 0, j = 0;
         int k = left;
         while (i < n1 && j < n2) {
@@ -34,15 +31,15 @@ class MergeSort {
             k++;
         }
     }
-    void sort(int[] arr, int left, int right) {
+    public static void Sort(int[] arr, int left, int right) {
         if (left < right) {
             int m = left + (right - left) / 2;
-            sort(arr, left, m);
-            sort(arr, m + 1, right);
-            merge(arr, left, m, right);
+            Sort(arr, left, m);
+            Sort(arr, m + 1, right);
+            Merge(arr, left, m, right);
         }
     }
-    static void printArray(int[] arr) {
+    public static void Print(int[] arr) {
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
@@ -50,11 +47,10 @@ class MergeSort {
     public static void main(String[] args) {
         int[] arr = { 14, 12, 15, 4, 6, 8 };
         System.out.println("Original array:");
-        printArray(arr);
-
+        Print(arr);
         MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length - 1);
+        ob.Sort(arr, 0, arr.length - 1);
         System.out.println("Sorted array:");
-        printArray(arr);
+        Print(arr);
     }
 }
